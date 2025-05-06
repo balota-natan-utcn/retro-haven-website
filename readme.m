949f94485759ee68787c844c3d90a96a37944ae1
Natan Balota <balotann61@gmail.com>
2025-05-06 09:51:24 +0300

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,2 +1,5 @@
 #retro-haven
 website for selling retro games/retro games ebay(pulled from private uni repo)
+
+backend uses express
+frontend is made using angular and has two user interfaces, client and admin, the admin dashboard can be used to create, remove, update and delete products(CRUD)
