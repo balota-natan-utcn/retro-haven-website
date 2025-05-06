8cdd8bd9e26810c1c37ab389cee9ee188eeacd37
Natan Balota <balotann61@gmail.com>
2025-05-06 09:40:25 +0300

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,14 +1,2 @@
-# Start backend
-npm run start
-
-# Reset to original commit
-git reset --hard origin/main 
-
-npm install -g @angular/cli
-
-ng new ui --ssr 
-Sass (SCSS), no server routing and App Engine APIs
-
-Chat: https://super-duper-engine-9r6xxxggq4hx9w-3000.app.github.dev/
-
-Dashboard: https://friendly-xylophone-6qvpppxjv42g9j-8080.app.github.dev/
+#retro-haven
+website for selling retro games/retro games ebay(pulled from private uni repo)
