e5e012e803c856fffb550ef8335b36aec5e8dd61
Natan Balota <balotann61@gmail.com>
2025-05-13 20:03:14 +0300

    Update readme.m

--- readme.m
+++ readme.m
@@ -3,3 +3,18 @@ website for selling retro games/retro games ebay(pulled from private uni repo)
 
 backend uses express
 frontend is made using angular and has two user interfaces, client and admin, the admin dashboard can be used to create, remove, update and delete products(CRUD)
+
+
+Steps to run it:
+you need Node.js and angular installed
+
+For the backend:
+  -"npm install -g nodemon"
+	-"npm install express"
+	-"npm run start" (to start the backend)
+For the client ui:
+  -"npm install"
+	-"ng serve"(to start the client ui)
+For the admin-dashboard:
+  -"npm install"
+	-"ng serve"(to start admin-dashboard)
